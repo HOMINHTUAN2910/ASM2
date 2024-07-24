26a64c03a9f6e48cc0687ed9ea1113a7d5cf58ec
tam986 <39400292+tam986@users.noreply.github.com>
2024-07-24 09:56:31 +0700

    Tam cap nhat
    24/7 9:56
    thêm file connect.php ở folder model

--- index.php
+++ index.php
@@ -1,4 +1,51 @@
 <?php
-include './view/header.php';
-include './view/home.php';
-include './view/footer.php';
+ob_start();
+session_start();
+include_once "model/connect.php";
+
+pdo_get_connection();
+
+include_once 'view/header.php';
+if (!isset($_GET['page'])) {
+
+  include "view/home.php";
+} else {
+  switch ($_GET['page']) {
+    case 'home':
+      include "view/home.php";
+      break;
+    case 'tamly':
+
+      break;
+    case 'detail':
+
+      break;
+      // case 'cotrang':
+      //   $categoryName = "Tâm lý";
+      //   $movies = get_film_buytheloai($categoryName);
+      //   include "view/cotrang.php";
+      //   break;
+    case 'tinhcam':
+
+      break;
+      // case 'bian':
+      //   $categoryName = "Tâm lý";
+      //   $movies = get_film_buytheloai($categoryName);
+      //   include "view/bian.php";
+      //   break;
+    case 'kinhdi':
+
+      break;
+    case 'hanhdong':
+
+      break;
+    case 'vothuat':
+      include "view/vothuat.php";
+    default:
+      include "view/404.php";
+      break;
+  }
+}
+
+include_once 'view/footer.php';
+ob_end_flush();
