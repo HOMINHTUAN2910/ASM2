202117e36202b2c528e51c195e04389b5950534a
HOMINHTUAN2910 <tuanhm2910@gmail.com>
2024-09-02 16:20:23 +0700

    new project this is only thing i can do tutorial hell again fogive me =(((

--- client/pages/components/Header.tsx
+++
@@ -1,8 +0,0 @@
-import { FaFacebook } from "react-icons/fa";
-export default function Header() {
-    return (
-      <header className="flex items-center h-[60px]">
-        <FaFacebook className="text-4xl text-blue-600 mx-3"/>
-      </header>
-    )
-  }
